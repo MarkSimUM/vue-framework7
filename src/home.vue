b9915acb4c2a7aef3c09491e967ccd10d805fa52
MarkBrightFutureApps <marksim@UMICH.EDU>
2016-11-21 11:24:08 -0500

    home.vue add text

--- src/home.vue
+++ src/home.vue
@@ -2,16 +2,23 @@
   div#app
     f7-views(navbar-through='')
       f7-view(main='', url='/', :dynamic-navbar='true')
-        f7-navbar
-          f7-nav-left
-          f7-nav-center(sliding='') Home
-          f7-nav-right
-        f7-pages#pages
-          f7-page.navbar-fixed
-            f7-list
-              f7-list-item(title='To page 1' link='/page1/')
-              f7-list-item(title='To page 2' link='/page2/')
-              f7-list-item(title='To page 3' link='/page3/')
+    
+          f7-navbar
+            f7-nav-left
+            f7-nav-center(sliding='') Home
+            f7-nav-right
+                       
+            
+          f7-pages#pages
+            f7-page.navbar-fixed
+              f7-block {{msg}}  
+              f7-list
+                f7-list-item(title='To page 1' link='/page1/')
+                f7-list-item(title='To page 2' link='/page2/')
+                f7-list-item(title='To page 3' link='/page3/')
+              f7-block This is the main page.
+                br
+                | Data-binding: {{msg}}                
 </template>
 
 <script>
