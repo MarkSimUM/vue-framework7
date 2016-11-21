8280b907b0f21c40d197944112af97865ff4727b
MarkBrightFutureApps <marksim@UMICH.EDU>
2016-11-21 11:09:08 -0500

    First commit

---
+++ src/page3.vue
@@ -0,0 +1,36 @@
+<template lang="jade">
+  f7-page(hide-bars-on-scroll='')
+    f7-navbar(back-link='Back', title='Page 3', sliding='')
+    f7-block This is page 3.
+      br
+      | Data-binding {{ mydata }}
+      p.red This text is 
+       span red
+      p.cursive This is 
+        span cursive
+</template>
+
+<script>
+export default {
+  name: 'page3',
+  data () {
+    return {
+      mydata: 'this is mydata'
+    }
+  }
+}
+</script>
+
+<style lang="sass?indentedSyntax">
+  // Add your styles here.
+  // See the following link for info on indented syntax:
+  // http://sass-lang.com/documentation/file.INDENTED_SYNTAX.html
+
+  .red
+    span
+      color: red
+
+  .cursive
+    span
+      font-style: italic
+</style>
